1275cf09056e28abea92933ba675a025e7877bb0
Saurabh Wankhade <saurabhwankhade2003@gmail.com>
2026-09-08 23:45:55 +0530

    Update portfolio design and add projects, resume and demo video

--- config/settings.py
+++ config/settings.py
@@ -143,11 +143,13 @@ STATICFILES_STORAGE = (
 
 
 # Security for production
-
-CSRF_TRUSTED_ORIGINS = os.getenv(
-    "CSRF_TRUSTED_ORIGINS",
-    ""
-).split(",")
-
+CSRF_TRUSTED_ORIGINS = [
+    origin.strip()
+    for origin in os.getenv(
+        "CSRF_TRUSTED_ORIGINS",
+        ""
+    ).split(",")
+    if origin.strip()
+]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
